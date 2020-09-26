d482d068d20c2defb58e2b41a65a6b14142e0b2e
Chad Altenberger <chadaltenberger@gmail.com>
2020-09-26 17:46:21 -0400

    Routing

--- app/components/Terms.js
+++ app/components/Terms.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function About() {
+function Terms() {
   return (
     <div className='container container--narrow py-md-5'>
-      <h2>About Us</h2>
+      <h2>Our Terms &amp; Conditions</h2>
       <p className='lead text-muted'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
         dolorum labore quisquam vel id dicta fuga! Ducimus, quo. Dolore commodi
@@ -24,8 +24,15 @@ function About() {
         nobis provident quo dolorum sapiente temporibus facere non repellendus
         consequatur cupiditate!
       </p>
+      <h3>Details</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae laboriosam
+        distinctio atque sint earum? Temporibus, voluptas aspernatur aliquam
+        nisi sed harum laborum, nemo odio animi officia quisquam. Veniam, natus
+        reprehenderit.
+      </p>
     </div>
   );
 }
 
-export default About;
+export default Terms;
